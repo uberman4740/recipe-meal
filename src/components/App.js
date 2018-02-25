8d4dd04df11cc0e3a6faac7eef16b8009f7ece0a
Karan Sagoo <ksagoo.ee@gmail.com>
2018-02-24 20:50:42 -0800

    Started Provider

--- src/components/App.js
+++ src/components/App.js
@@ -4,41 +4,14 @@ import "../App.css";
 import { addRecipe } from "../actions/index";
 
 class App extends Component {
-  state = {
-    calendar: null
-  };
-  componentDidMount() {
-    const { store } = this.props;
-    store.subscribe(() => {
-      this.setState(() => ({ calendar: store.getState() }));
-    });
-  }
-  submitFood = () => {
-    this.props.store.dispatch(
-      addRecipe({
-        day: 'monday',
-        meal: 'breakfast',
-        recipe: {
-          label: this.input.value
-        }
-      })
-    );
-    this.input.value = "";
-  };
+
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          ref={(input) => this.input = input}
-          placeholder="Mondays breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
-      </div>
-    );
+        <div>
+            Starting with provider
+        </div>
+    )
+
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore } from "redux";
 import reducer from "./reducers";
+import { Provider } from "react-redux";
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -12,5 +13,11 @@ const store = createStore(
 
 // console.log(store);
 
-ReactDOM.render(<App store={store} />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
 registerServiceWorker();
